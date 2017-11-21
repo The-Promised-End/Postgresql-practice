de015fa957ab27aeef53a889f73b4b2719582e05
The-Promised-End <tom.tk.li@gmail.com>
2017-11-21 20:57:42 +0000

    Refactored code

--- lookup_people.js
+++ lookup_people.js
@@ -1,30 +1,32 @@
-var myArgs = process.argv.slice(2);
 const pg = require("pg");
-const settings = require("./settings"); // settings.json
+const settings = require("./settings");
+
+const args = process.argv.slice(2);
 
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl
 });
 
-function lookupPeopleByFirstOrLastName(name) {
-  client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", [myArgs[0]], (err, result) => {
-    if (err) {
-      console.error("Error running query! ", err)
-    } else {
-      console.log(result.rows[0]);
-      client.end();
-    }
-  });
-};
+function lookupPeopleByFirstOrLastName(name, cb) {
+  client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", [name], cb);
+}
 
 client.connect((err) => {
   if (err) {
-    console.error("Connection error. ", err)
+    console.error("Connection error. ", err);
+    return;
   }
-  lookupPeopleByFirstOrLastName(myArgs[0]);
+  lookupPeopleByFirstOrLastName(args[0], (err, result) => {
+    if (err) {
+      console.error("Error running query! ", err);
+      return;
+    }
+    console.log(result.rows[0]);
+    client.end();
+  });
 });
